3806d1eb1e8b566aac3a2d81856ff4beb9670a71
Nicole Aptekar <nicoletbn@gmail.com>
2013-08-03 21:32:41 -0700

    New rails project WOO.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Serendipities::Application.config.secret_key_base = 'a798cbd948a6b70093b2c08103fa3dc5ad577f3f4d3e381daace1a8b3d86dc68cc6deac6a97724eab3196da299c0064a059a2c76a220ff5db6b42dbb87638ab0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Serendipities::Application.config.session_store :cookie_store, key: '_serendipities_session'
