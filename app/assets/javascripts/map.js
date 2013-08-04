9c2502960d9893e384e9a9a25bdcba45a28a3850
Nicole Aptekar <nicoletbn@gmail.com>
2013-08-04 02:21:58 -0700

    Tracks! Omg! They exist!

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -3,6 +3,30 @@ Map = {};
 Map.initialize = function(){
   this.map = L.map('map').setView([37.775, -122.418], 13);
   L.tileLayer.provider('Stamen.Toner').addTo(this.map);
+
+  var points = [];
+  data[0].segments.forEach(function(segment){
+    var lat, lng;
+    if (segment.place){
+      lat = segment.place.location.lat;
+      lng = segment.place.location.lon;
+      points.push(new L.LatLng(lat,lng));
+    }
+    if (segment.activities){
+      segment.activities.forEach(function(activity){
+        activity.trackPoints.forEach(function(trackPoint){
+          lat = trackPoint.lat;
+          lng = trackPoint.lon;
+          points.push(new L.LatLng(lat,lng));
+        });
+      });
+    }
+  });
+  // create a red polyline from an arrays of LatLng points
+  var polyline = L.polyline(points, {color: 'red'}).addTo(this.map);
+
+  // zoom the map to the polyline
+  // this.map.fitBounds(polyline.getBounds());
 };
 
 
