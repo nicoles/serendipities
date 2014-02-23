6f1a992a931d60f9e1b657b99dcacfa42976decf
Serene H <keroserene+git@gmail.com>
2014-02-23 01:02:57 -0500

    cacheinfo route

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Serendipities::Application.routes.draw do
   get '/logout', to: 'sessions#destroy', as: 'logout'
   get '/mapdata', to: 'mapdata#show', as: 'mapdata'
   get '/admin', to: 'admin#show', as: 'admin'
+  get '/cacheinfo', to: 'mapdata#cache', as: 'cacheinfo'
   root to: 'homepage#show'
 end
