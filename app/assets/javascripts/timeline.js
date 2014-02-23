383f90b5ffb1a92225bb7f8ce5f043b10e62189d
Serene H <keroserene+git@gmail.com>
2014-02-22 21:17:09 -0500

    prepare moves query event and custom time event

--- app/assets/javascripts/timeline.js
+++ app/assets/javascripts/timeline.js
@@ -1,31 +1,49 @@
 /*
   Render and hook up timeline to moves.
 */
-var data = [];
-function push(year, month, day, label) {
-  data.push({
-    'start': new Date(year, month, day),
-    'content': label
-  });
-}
 
-var options = {
-  height: '88px',
-  'width': '100%',
-  'editable': true
-};
+// Obtain a singleton event for the MOVES query.
+function movesRange(year, month, day) {
+  return {
+    // 'className': 'moves-event',
+    'start': new Date(year, month, day),
+    'end': new Date(year, month+1, day),
+    'content': 'moves query',
+    'editable': true,
+    'dragAreaWidth': 30,
+    'animate': false
+  };
+}
 
 $(function() {
 
-  push(2012, 0, 1, 'lol');
-  push(2013, 0, 1, 'wat');
+  data = [];
+  data.push(movesRange(2014, 0, 1));
 
+  // Tweaking the timeline interaction to make sense with moves.
+  var options = {
+    'width': '100%',
+    'zoomMin': 36000000, // Minimum is 1 hour (in ms)
+    'snapEvents': false,
+    'style': 'dot',
+    'showCustomTime': true,
+    'unselectable': false
+  };
   var $timeline = $('#timeline')[0];
 
   // Assumes timeline-min.js has been loaded, and the global var |timeline| is
   // already the correct DOM element.
   // (http://almende.github.io/chap-links-library/js/timeline/doc/)
   var timeline = new links.Timeline($timeline);
+  timeline.setCustomTime(new Date());
 
+  // links.events.addListener(timeline, 'select', function() {
+    // var sel = timeline.getSelection();
+    // console.log(sel);
+  // })
+  var defaultSelection = [{row: 0}];  // First element is the MOVES query.
+
+  // Render everything.
   timeline.draw(data, options);
-}); 
+  timeline.setSelection(defaultSelection);
+});
