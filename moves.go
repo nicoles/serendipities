a8269beffe1a69755b4f3bce88e54d6576055798
Serene Han <keroserene+git@gmail.com>
2016-06-07 18:56:45 -0700

    Can has token. Go basic auth with Moves works [finishes #121095107]

--- moves.go
+++ moves.go
@@ -1,117 +1,131 @@
 package main
 
 import (
-  "golang.org/x/oauth2"
-  "fmt"
-  "os"
-  // "io/ioutil"
-  // "flag"
-  "html/template"
-  "net/http"
-  // "log"
+	"encoding/json"
+	"fmt"
+	"golang.org/x/oauth2"
+	"html/template"
+	"net/http"
+	"os"
+)
+
+const (
+	MovesAuthURL     = "https://api.moves-app.com/oauth/v1/authorize"
+	MovesTokenURL    = "https://api.moves-app.com/oauth/v1/access_token"
+	MovesTokenURLFmt = "https://api.moves-app.com/oauth/v1/access_token?grant_type=authorization_code&code=%s&client_id=%s&client_secret=%s"
 )
 
 var movesAuthURL string = ""
+var config *oauth2.Config
+var code string
+var token *oauth2.Token
 
 func check(err error) {
-  if nil != err {
-    panic(err)
-  }
+	if nil != err {
+		panic(err)
+	}
 }
 
-/*
-func loadPage(title string) (*Page, error) {
-    filename := title + ".txt"
-    body, err := ioutil.ReadFile(filename)
-    if err != nil {
-        return nil, err
-    }
-    return &Page{Title: title, Body: body}, nil
+func getToken() (*oauth2.Token, error) {
+	tokenURL := setTokenURL(code, config.ClientID, config.ClientSecret)
+	config.Endpoint.TokenURL = tokenURL
+	// Obtain moves token.
+	received, er := config.Exchange(oauth2.NoContext, code)
+	if nil != er || nil == received {
+		return nil, er
+	}
+	token = received
+	code = ""
+	return token, nil
 }
-*/
+
+// For some reason, Moves requires the query string with the POST...
+func setTokenURL(code, id, secret string) string {
+	return fmt.Sprintf(MovesTokenURLFmt, code, id, secret)
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
-  // fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
-  // fmt.Fprintf(w, movesAuthUrl)
-  data := struct{
-    MovesURL string
-  }{
-    movesAuthURL,
-  }
-  fmt.Println(data)
-  t, _ := template.ParseFiles("index.html")
-  t.Execute(w, data)
+	url := config.AuthCodeURL("state")
+	fmt.Println("\nUsing Moves API URL: \n", url)
+	data := struct {
+		MovesURL string
+		Token    *oauth2.Token
+	}{
+		url, token,
+	}
+	fmt.Println(data)
+	t, _ := template.ParseFiles("templates/index.html")
+	t.Execute(w, data)
 }
 
 // Callback once moves has authorized.
 func authHandler(w http.ResponseWriter, r *http.Request) {
-  params := r.URL.Query()
-  codes, ok := params["code"]
-  if !ok || nil == codes {
-    w.WriteHeader(http.StatusBadRequest)
+  if nil != token {
+    fmt.Fprintf(w, "Already authorized with Moves.")
     return
   }
-  state, ok := params["state"]
-  if !ok || nil == state {
-    fmt.Fprintf(w, "Invalid State")
-    w.WriteHeader(http.StatusBadRequest)
+
+	// Check for auth error.
+	errors := r.FormValue("errors")
+	if "" != errors {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	state := r.FormValue("state")
+	if state != "state" {
+		fmt.Fprintf(w, "Invalid State")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	code = r.FormValue("code")
+	http.Redirect(w, r, "/gettoken", 301)
+}
+
+func tokenHandler(w http.ResponseWriter, r *http.Request) {
+  if nil != token {
+    fmt.Fprintf(w, "Already authorized with Moves.")
     return
   }
-  code := codes[0]
-  fmt.Fprintf(w, "Auth Code:  " + code)
+	if "" == code {
+		fmt.Fprintf(w, "No auth code yet.")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	token, err := getToken()
+	if nil == token {
+		fmt.Fprintf(w, "Problem getting token.\n")
+		fmt.Fprintf(w, err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	b, _ := json.Marshal(token)
+	fmt.Fprintf(w, "Moves Token:  "+string(b))
 }
 
 func main() {
 
-  // key := flag.String("moves_key", "", "Moves Key")
-  // secret := flag.String("moves_secret", "", "MovesSecret")
-  // flag.Parse()
-  // getParams()
-  key := os.Getenv("MOVES_KEY")
-  secret := os.Getenv("MOVES_SECRET")
-  fmt.Println("Moves Key: ", key)
-  fmt.Println("Moves Secret: ", secret)
+	key := os.Getenv("MOVES_KEY")
+	secret := os.Getenv("MOVES_SECRET")
+	fmt.Println("Moves Key: ", key)
+	fmt.Println("Moves Secret: ", secret)
 
-  conf := &oauth2.Config{
-    ClientID:     key,
-    ClientSecret: secret,
-    Scopes: []string{"activity location"},
-    Endpoint: oauth2.Endpoint{
-      AuthURL:  "",
-      TokenURL: "",
-    },
-  }
-  fmt.Println("conf:", conf)
+	config = &oauth2.Config{
+		// config = {
+		ClientID:     key,
+		ClientSecret: secret,
+		Scopes:       []string{"activity location"},
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  MovesAuthURL,
+			TokenURL: MovesTokenURL,
+		},
+	}
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/auth/moves/callback", authHandler)
+	http.HandleFunc("/gettoken", tokenHandler)
 
-  moves_url := "https://api.moves-app.com/oauth/v1/authorize"
+	fmt.Println("Serving on localhost:3000...")
+	http.ListenAndServe(":3000", nil)
 
-  // url := conf.AuthCodeURL("state", oauth2.AccessTypeOffline)
-  url := moves_url + conf.AuthCodeURL("state")
-  movesAuthURL = url
-
-  fmt.Println("\nMoves API URL: ", url)
-
-  http.HandleFunc("/", handler)
-  http.HandleFunc("/auth/moves/callback", authHandler)
-
-  fmt.Println("Serving on localhost:3000...")
-  http.ListenAndServe(":3000", nil)
-
-  // Use the authorization code that is pushed to the redirect URL.
-  // NewTransportWithCode will do the handshake to retrieve
-  // an access token and initiate a Transport that is
-  // authorized and authenticated by the retrieved token.
-  /*
-  var code string
-  if _, err := fmt.Scan(&code); err != nil {
-      log.Fatal(err)
-  }
-  tok, err := conf.Exchange(oauth2.NoContext, code)
-  if err != nil {
-      log.Fatal(err)
-  }
-  
-  client := conf.Client(oauth2.NoContext, tok)
-  client.Get("...")
-  */
+	// client := conf.Client(oauth2.NoContext, tok)
+	// client.Get("...")
 }
