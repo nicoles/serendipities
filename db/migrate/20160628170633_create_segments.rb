a44dabb97157b4b8a01fa922b5ef096635958b46
Nicole Aptekar <me@nicolation.net>
2016-07-03 15:47:30 -0700

    clarify a few things, better follow moves api

--- db/migrate/20160628170633_create_segments.rb
+++ db/migrate/20160628170633_create_segments.rb
@@ -5,7 +5,7 @@ class CreateSegments < ActiveRecord::Migration
       t.datetime :end_time, null: false
       t.datetime :last_update
       t.integer  :storyline_id, null: false
-      t.boolean  :move, null: false
+      t.string   :segment_type, null: false
       t.integer  :place_id #can be null, when move is true
 
       t.timestamps
--- db/migrate/20160629152824_create_activities.rb
+++ db/migrate/20160629152824_create_activities.rb
@@ -4,8 +4,8 @@ class CreateActivities < ActiveRecord::Migration
       t.datetime :start_time
       t.datetime :end_time
       t.integer  :segment_id, null: false
-      t.string   :type, null: false
-      t.string   :group
+      t.string   :activity_type, null: false
+      t.string   :activity_group
       t.integer  :duration
       t.integer  :distance
       t.integer  :calories
