cdd82830dc34f021ef71b83a11ba40c4373dc0f3
Nicole Aptekar <me@nicolation.net>
2016-07-26 15:23:20 -0700

    initial testing (not quite working yet)

--- spec/controllers/mapdata_controller_spec.rb
+++ spec/controllers/mapdata_controller_spec.rb
@@ -1,5 +1,33 @@
-require 'spec_helper'
+require 'rails_helper'
+include AuthenticationHelper
 
 describe MapdataController do
+  describe '#show' do
+    let(:start_date) { 4.days.ago.to_date.to_s }
+    let(:end_date) { 4.days.ago.to_date.to_s }
+
+    context 'when not signed in' do
+      it 'redirects to login' do
+        get :show, start_date: start_date, end_date: end_date
+
+        expect(response).to redirect_to('/auth/moves')
+      end
+    end
+
+    context 'when signed in' do
+      before do
+        # get signed in. who knows
+        user = User.last
+        sign_in user
+      end
+
+      it 'returns geojson for a pair of dates' do
+        get :show, start_date: start_date, end_date: end_date
+
+        expect(response).to be_success
+        parsed_body = JSON.parse(response.body)
+      end
+    end
+  end
 
 end
--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe Place do
   pending "add some examples to (or delete) #{__FILE__}"
